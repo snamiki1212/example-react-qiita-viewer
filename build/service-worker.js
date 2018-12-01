68f6ebdde7b5cf09c17f807db583f52751cdd8a6
snamiki1212 <snamiki1212@gmail.com>
2018-12-01 09:54:44 +0900

    Update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cea39c906c218660c02df1e6e579b1aa.js"
+  "/qiita-viewer-with-react/precache-manifest.a333f82e4866132f50ee49c58e2bb93c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/qiita-viewer-with-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
