4791a2277ac2daa675f7932e038673f9ac2841fd
snamiki1212 <snamiki1212@gamil.com>
2018-12-01 09:06:32 +0900

    Updates

---
+++ precache-manifest.7f6591c1e8d1f53866da6b50bb56ce4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "576c759aeef7931bbe69",
+    "url": "/qiita-viewer-with-react/build/static/js/runtime~main.576c759a.js"
+  },
+  {
+    "revision": "74be7b894d1e773ea7af",
+    "url": "/qiita-viewer-with-react/build/static/js/main.74be7b89.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/qiita-viewer-with-react/build/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "74be7b894d1e773ea7af",
+    "url": "/qiita-viewer-with-react/build/static/css/main.4e9b55eb.chunk.css"
+  },
+  {
+    "revision": "f49c496033298c2493ae16e1cbd66013",
+    "url": "/qiita-viewer-with-react/build/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/qiita-viewer-with-react/build/precache-manifest.7f6591c1e8d1f53866da6b50bb56ce4f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/qiita-viewer-with-react/build/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
